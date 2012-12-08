01128e5644259402d3b27c124d64a044c40058a9
Marquett Davon Burton <marquett@hobbybuddy.com>
2012-12-08 18:58:39 -0500

    post shane

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -39,8 +39,8 @@ describe "Authentication" do
   describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
+        fill_in "session_email",    with: user.email
+        fill_in "session_password", with: user.password
         click_button "Go!"
       end
 
